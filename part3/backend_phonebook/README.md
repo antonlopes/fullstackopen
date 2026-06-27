32cfd44adbb712f0c059f6ffb2c9173fc35bcb43
Anton Lopes <antoniollopescorrea@gmail.com>
2026-06-26 21:19:13 -0300

    Add backend_phonebook as regular directory

---
+++ part3/backend_phonebook/README.md
@@ -0,0 +1,6 @@
+# Full Stack Open — Phonebook Backend
+
+The application is available online at:
+
+
+https://fso-phonebook-backend-671x.onrender.com
---
+++ part3/backend_phonebook/index.js
@@ -0,0 +1,123 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+const Person = require('./models/person')
+require('dotenv').config()
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static('build'))
+
+morgan.token('request-body', (req) => {
+  if (req.method === 'POST' && req.body) {
+    return JSON.stringify(req.body)
+  }
+  return '-'
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request-body'))
+
+
+
+app.get('/api/persons', (req, res, next) => {
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
+    .catch(error => next(error))
+})
+
+
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+
+app.get('/info', (req, res, next) => {
+  const now = new Date()
+
+  Person.find({}).then(persons => {
+    const entriesCount = persons.length
+
+    res.send(`
+            <p>Phonebook has info for ${entriesCount} people</p>
+            <p>${now}</p>
+        `)
+  })
+    .catch(error => next(error))
+
+})
+
+
+
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+
+
+app.post('/api/persons', (req, res, next) => {
+  const body = req.body
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save()
+    .then(savedPerson => {
+      res.json(savedPerson)
+    })
+    .catch(error => next(error))
+})
+
+
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const { name, number } = req.body
+
+  Person.findByIdAndUpdate(
+    req.params.id,
+    { name, number },
+    { returnDocument: 'after' , runValidators: true, context: 'query' }
+  )
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+
+app.use(errorHandler)
+
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
