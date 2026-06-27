10c1bbc81e3ee838dec98620848c83399eade37b
Anton Lopes <antoniollopescorrea@gmail.com>
2026-06-26 21:04:39 -0300

    Fixes

---
+++ part3/frontend_phonebook/src/App.js
@@ -0,0 +1,204 @@
+import { useState, useEffect } from 'react'
+import Filter from "./components/Filter"
+import PersonForm from "./components/PersonForm"
+import Persons from "./components/Persons"
+import personService from "./services/persons"
+import Notification from './components/Notification'
+import './index.css'
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState({
+    name: '',
+    number: ''
+  })
+  const [search, setSearch] = useState('')
+  const [notificationMessage, setNotificationMessage] = useState(null)
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+      })
+  }, [])
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+
+    setNewName({
+      ...newName,
+      [name]: value
+    })
+  }
+
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value)
+  }
+
+  const addContact = (event) => {
+    event.preventDefault()
+
+    if (newName.name.trim() === '' || newName.number.trim() === '') {
+      alert('Name and number are required')
+      return
+    }
+
+    const existingPerson = persons.find(
+      person => person.name.toLowerCase() === newName.name.trim().toLowerCase()
+    )
+
+    if (existingPerson) {
+      alert(`${existingPerson.name} is already added to phonebook`)
+      return
+    }
+
+    // if (existingPerson) {
+    //   const confirmUpdate = window.confirm(
+    //     `${existingPerson.name} is already added to phonebook, replace the old number with a new one?`
+    //   )
+
+    //   if (!confirmUpdate) {
+    //     return
+    //   }
+
+    //   const changedPerson = {
+    //     ...existingPerson,
+    //     number: newName.number.trim()
+    //   }
+
+    //   personService
+    //     .update(existingPerson.id, changedPerson)
+    //     .then(returnedPerson => {
+    //       setPersons(persons.map(person =>
+    //         person.id !== existingPerson.id ? person : returnedPerson
+    //       ))
+
+    //       setNotificationMessage({
+    //         message: `${returnedPerson.name} was changed on the list.`,
+    //         type: 'success'
+
+    //       })
+
+    //       setTimeout(() => {
+    //         setNotificationMessage(null)
+    //       }, 5000)
+
+    //       setNewName({
+    //         name: '',
+    //         number: ''
+    //       })
+    //     })
+    //     .catch(error => {
+    //       setNotificationMessage({
+    //         message: `${changedPerson.name} has already been removed from server.`,
+    //         type: 'error'
+    //       })
+
+    //       setPersons(persons.filter(person => person.id !== existingPerson.id))
+
+    //       setTimeout(() => {
+    //         setNotificationMessage(null) 
+    //       }, 5000)
+    //     })
+    //   return
+    // }
+
+  // devo atualizar o catch error do update (personservice)?
+
+
+    const personObject = {
+      name: newName.name.trim(),
+      number: newName.number.trim()
+    }
+
+    personService
+      .create(personObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))       
+      
+        setNotificationMessage({
+          message: `${returnedPerson.name} has been added to the list.`,
+          type: 'success'
+        })
+
+        setTimeout(() => {
+          setNotificationMessage(null)
+        }, 5000)
+        
+        setNewName({
+          name: '',
+          number: ''
+        })
+      })
+      .catch(error => {
+        setNotificationMessage({
+          message: error.response?.data?.error || 'Could not add the person.',
+          type: 'error'
+        })
+        setTimeout(() => {
+          setNotificationMessage(null)
+        }, 5000)
+      })
+  }
+
+  const handleDelete = (id) => {
+    const person = persons.find(person => person.id === id)
+
+    if (!person) {
+      return
+    }
+
+    const confirmDelete = window.confirm(`Delete ${person.name}?`)
+
+    if (!confirmDelete) {
+      return
+    }
+
+    personService
+      .remove(id)
+      .then(() => {
+        setPersons(persons.filter(person => person.id !== id))
+      })
+      .catch(error => {
+        setNotificationMessage({
+            message: `Information of ${person.name} has already been removed from server.`,
+            type: 'error'
+          })
+
+         setPersons(persons.filter(person => person.id !== id))
+
+          setTimeout(() => {
+            setNotificationMessage(null) 
+          }, 5000)
+      })
+  }
+
+  return (
+    <div>
+      <h2>Phonebook Test</h2>
+      <Notification notification={notificationMessage}/>
+
+      <Filter
+        searchChange={handleSearchChange}
+        search={search}
+      />
+
+      <PersonForm
+        handleChange={handleChange}
+        addContact={addContact}
+        newName={newName}
+      />
+
+      <h2>Numbers</h2>
+
+      <Persons
+        persons={persons}
+        search={search}
+        onDelete={handleDelete}
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ part3/frontend_phonebook/src/components/Person.js
@@ -0,0 +1,13 @@
+const Person = ({ person, onDelete }) => {
+  return (
+    <>
+      <p>
+        {person.name} {person.number} <button onClick={() => onDelete(person.id)}>delete</button>
+      </p>
+      
+    </>
+    
+  )
+}
+
+export default Person
